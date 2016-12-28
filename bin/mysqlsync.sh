865286c0ca086f4e2d0768112f200291e1744c1f
Pierre Tomasina <tomasinap@gmail.com>
2016-12-28 15:50:40 +0100

    Daemonzie mysqlsync schema

--- bin/mysqlsync.sh
+++ bin/mysqlsync.sh
@@ -1,16 +1,38 @@
 #!/bin/bash -
 #title           :mysqlsync
 #description     :Synchronize MYSQL DB Schema between two databases
-#author		 :© 2016 Continuous SA - Tomasina Pierre
+#author          :© 2016 Continuous SA - Tomasina Pierre
 #date            :20161219
 #version         :0.1
-#usage		 :bash mysqlsync
+#usage           :bash mysqlsync
 #notes           :Requirement : git, mysqlclient, ruby2.4
 #==============================================================================
 
 pushd `dirname $0`/../ > /dev/null
 MYSQLSYNC_PATH=`pwd -P`
 popd > /dev/null
+
+_term() {
+    echo -e "${PINK}Caught SIGTERM signal!${NORMAL}"
+    _SIGTERM="defined"
+}
+
+trap _term SIGTERM
+
+_foreground()
+{
+    if [ -n "$_SIGTERM" ]
+    then
+        exit 3
+    fi
+
+    if [ -n "$iflag" ]
+    then
+        sleep $respawn
+    else
+        exit 0
+    fi
+}
 
 GREEN="\\033[1;32m"
 NORMAL="\\033[0;39m"
@@ -33,8 +55,9 @@ function usage {
   -h\t Displays this message.
   -b\t mysqlbinlog binary. (Default at /usr/bin/mysqlbinlog)
   -d\t mysqldiff binary. (Default at /usr/local/src/mysqldiff/bin/mysqldiff.sh)
-  -r\t Respawn time to wait before reconnecting after failure.
   -c\t Credentials of Databases in string format \"${BLUE}SRC_HOST=x SRC_USER=x SRC_PASSWORD=x DST_HOST=x DST_USER=x DST_PASSWORD=x${NORMAL}\"
+  -i\t Run the action infinitely with pause during respwan time between each execution
+  -r\t Respawn time in minute to wait before re execution (use with -i default to 60).
 
 \033[1mACTIONS\033[0m
   schema\t Update DST to have same mysql structure as SRC
@@ -52,6 +75,7 @@ bail ()
   usage
   exit 
 }
+
 
 credential_src_check()
 {
@@ -93,17 +117,18 @@ credential_dst_check()
 
 mbl=/usr/bin/mysqlbinlog
 mysqldiffbin=/usr/local/src/mysqldiff/bin/mysqldiff.sh
-respawn=10 # time to wait before reconnecting after failure
+respawn=3600
 
-while getopts ":hb:r:c:" opt; do
+while getopts ":hb:c:ir:" opt; do
 	case $opt in
 	  h) usage && exit 0 ;;
 	  b) mbl="$OPTARG" ;;
 	  d) mysqldiffbin="$OPTARG" ;;
-	  r) respawn="$OPTARG" ;;
 	  c) export $OPTARG
-		credential_src_check
+          credential_src_check
 	     ;;
+      i) iflag="defined" ;;
+      r) let respawn=60*$OPTARG ;;
 	  \?) echo -e "${RED}Invalid option: -$OPTARG${NORMAL}" >&2 ; bail 1 ;;
 	  :)  echo "Option -$OPTARG requires an argument." >&2 ; bail 2 
 	esac
@@ -117,11 +142,6 @@ if [[ ! $respawn =~ ^[1-9][0-9]*$ ]]
 	bail 2
 fi
 
-if [ ! -x $mbl ]
-  then
-	echo -e "Option -b mysqlbinlog programm not found at $mbl."
-	bail 2
-fi
 
 
 ######
@@ -141,13 +161,53 @@ if [ "schema" == "$1" ]
 		echo -e "Option -d mysqldiff programm not found at $mysqldiffbin."
 		bail 2
 	fi
-	credential_src_check
+
+    credential_src_check
 	credential_dst_check
-	echo -e "${BLUE}Synchronise schema from $SRC_HOST to $DST_HOST...${NORMAL}"
-	diff_schema
-	diff_raw_sql "primary"
-	diff_raw_sql "index"
-	diff_apply_change
+
+    while true
+    do
+        echo -e "${BLUE}Synchronise schema from $SRC_HOST to $DST_HOST...${NORMAL}"
+	    diff_schema
+	    diff_raw_sql "primary"
+	    diff_raw_sql "index"
+	    diff_apply_change
+
+        _foreground
+        if [ "3" == "$?" ]
+        then
+            echo "${PINK}I've been terminated.${NORMAL}"
+            exit 0
+        fi
+    done
 fi
+
+if [ "default" == "$1" ]
+  then
+	echo "Default not yet implemented"
+    exit 0
+fi
+
+if [ "ebinlog" == "$1" ]
+  then
+    if [ ! -x $mbl ]
+      then
+        echo -e "Option -b mysqlbinlog programm not found at $mbl."
+        bail 2
+    fi
+
+    while true
+    do
+        echo "Export binlog..."
+
+        _foreground
+        if [ "3" == "$?" ]
+        then
+            echo "${PINK}I've been terminated.${NORMAL}"
+            exit 0
+        fi
+    done
+fi
+
 
 
